86c805822852a1eb0a2874573d61d7a8c54fc9d7
Meshack Korir <meshackkorir100@gmail.com>
2023-11-25 15:22:58 +0300

    Navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-        <nav className="navbar-container">
-          <ul className="navbar-links">
-            <li>
-              <Link  to="/">Home</Link>
-            </li>
-            
-            <li>
-              <Link to="/parcel">Create parcel</Link>
-            </li> 
-            <li>
-              <Link to="/about">About </Link>
-              <li>
-              <Link to="/tracking">Track </Link>
-            </li> 
-            <li>
-              <Link to="/services">Services</Link>
-            </li> 
-            </li> 
-          </ul> 
-        </nav>
-    </div>
-  )
-}
+      <nav className="navbar-container">
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-export default Header
+          <li>
+            <Link to="/parcel">Create parcel</Link>
+          </li>
+
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+
+          <li>
+            <Link to="/tracking">Track</Link>
+          </li>
+
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
