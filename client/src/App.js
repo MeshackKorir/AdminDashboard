e572a469022eac2732fb6d1a4143c4b750a44d20
Meshack Korir <meshackkorir100@gmail.com>
2023-11-27 22:29:24 +0300

    service css

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Home from "./components/Home";
 import Parcel from "./components/Parcel";
 import About from "./components/About";
+import Services from "./components/Services";
 import { BrowserRouter,Routes, Route} from 'react-router-dom';
 
 
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={< Home/>}/>
           <Route path="/Parcel" element={<Parcel />}/>
           <Route path="/about" element={<About />} />
-          <Route path="/Services" element={<Services />}/>
+          <Route path="/services" element={<Services />}/>
         </Routes>
       </BrowserRouter>
     </div>
